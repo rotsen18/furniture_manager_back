12b8bea59696f0796785f6db1efd9d03b137fa78
t.nester <t.nester@disoft.dev>
2023-05-27 14:03:32 +0300

    add country code to address model

--- apps/directory/models/address.py
+++ apps/directory/models/address.py
@@ -1,15 +1,17 @@
 from django.db import models
 
+from mixins.models import SingleNameMixin
 
-class City(models.Model):
-    name = models.CharField(max_length=255)
+
+class City(SingleNameMixin):
+    pass
 
     def __str__(self):
         return self.name
 
 
-class Country(models.Model):
-    name = models.CharField(max_length=255)
+class Country(SingleNameMixin):
+    code = models.CharField(max_length=2, unique=True, default='')
 
     def __str__(self):
         return self.name
